fa3e920864eea93597d9489da5ecd67dca4e9696
Albert Brannan <abrannan5@gmail.com>
2024-04-13 21:37:22 -0400

    import polygon

--- Backend/server.py
+++ Backend/server.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from polygon import RESTClient
 
 app = Flask(__name__)
+
+client = RESTClient(api_key="sSdMfHNF6plcQt7FUUdkpo48gwcrnp6G")
 
 @app.route("/")
 def hello_world():
